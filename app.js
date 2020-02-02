907e1c200be53c8e4563a3fd0ccfcf25f1d4ca55
JaSpIM <imfkntm55@gmail.com>
2020-02-02 18:17:51 +0900

    練習

---
+++ app.js
@@ -0,0 +1,66 @@
+
+// navのくるっと
+
+$(function() {
+    $('#nav li')
+    .each(function(){
+        $(this).append('<span>'+$(this).html()+'</span>')
+    })
+     .hover(
+        function(){
+            $('a', this).stop().animate({
+                marginTop:'-1.5em'
+            },200);
+        },
+        function () {
+            $('a', this).stop().animate({
+                marginTop:'0em'
+            },300);
+        }
+    );
+});
+
+
+//■page topボタン
+
+$(function(){
+    var topBtn=$('#pageTop');
+    topBtn.hide();
+    
+     
+    
+    //◇ボタンの表示設定
+    $(window).scroll(function(){
+      if($(this).scrollTop()>80){
+    
+        //---- 画面を80pxスクロールしたら、ボタンを表示する
+        topBtn.fadeIn();
+    
+      }else{
+    
+        //---- 画面が80pxより上なら、ボタンを表示しない
+        topBtn.fadeOut();
+    
+      }
+    });
+    
+     
+    
+    // ◇ボタンをクリックしたら、スクロールして上に戻る
+    topBtn.click(function(){
+      $('body,html').animate({
+      scrollTop: 0},500);
+      return false;
+    
+    });
+    
+    
+    });
+
+
+
+
+
+
+
+
